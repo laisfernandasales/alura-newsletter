2b1ecc2fb4900fcda87882a2a4bbe83abe2938cc
lais <lais.melofernanda15gmail.com>
2025-02-22 14:02:18 +0000

    instalação da biblioteca HeroiIcons e implementação do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
   
-        <h1 className="text-3xl font-bold underline bg-black text-white">
-          Hello world!
-        </h1>
-     
-  )
+
+  
+   return (<Header />)
+ 
 }
 
 export default App
