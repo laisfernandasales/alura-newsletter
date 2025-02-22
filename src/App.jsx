1485d5e89c5ed1bb87bd9461e30c106d87a4bf27
lais <lais.melofernanda15gmail.com>
2025-02-22 13:58:02 +0000

    inicio do projeto instalação do vite e tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+        <h1 className="text-3xl font-bold underline bg-black text-white">
+          Hello world!
+        </h1>
+     
+  )
+}
+
+export default App
